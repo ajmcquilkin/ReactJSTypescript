2a4bab1079fb3266e73c9bc053f622e1aad33769
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-06-05 13:22:35 -0400

    Working react-router-dom setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 
-const App = () => (<div>Hello, world!</div>);
+const Welcome = () => (
+  <div>
+    <div>Hello, world!</div>
+    <NavLink to="/test">Test</NavLink>
+  </div>
+);
+
+const Test = () => (
+  <div>
+    This is a test of
+    {' '}
+    <code>react-router-dom</code>
+    <NavLink to="/">Home</NavLink>
+  </div>
+);
+
+const Fallback = () => (
+  <div>
+    Page not found
+    <NavLink to="/">Home</NavLink>
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <div id="app-container">
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/test" component={Test} />
+        <Route component={Fallback} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
