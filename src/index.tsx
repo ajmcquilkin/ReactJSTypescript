2affa90e979a524a1e63cd3e35f4beba82087a8d
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-06-05 12:22:52 -0400

    Initial working app (open index.html)

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,13 @@
-console.log('1');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import thunk from 'redux-thunk';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddeware, compose } from 'redux';
+
+// import reducers from './reducers';
+import App from './components/app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('main'),
+);
