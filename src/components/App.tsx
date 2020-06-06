bc8a0ec9e019f3337f51848378c7008a3fe44a68
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-06-05 21:09:29 -0400

    Fixed error with props

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,35 +3,46 @@ import {
   BrowserRouter as Router, Route, NavLink, Switch,
 } from 'react-router-dom';
 
-const Welcome = () => (
+interface WelcomeProps {
+
+}
+
+const Welcome: React.FC = () => (
   <div>
     <div>Hello, world!</div>
     <NavLink to="/test">Test</NavLink>
   </div>
 );
 
-const Test = () => (
+interface TestProps {
+  testString: string;
+}
+
+const Test: React.FunctionComponent<TestProps> = ({ testString }: TestProps) => (
   <div>
     This is a test of
     {' '}
     <code>react-router-dom</code>
+    with test prop
+    {' '}
+    {testString}
     <NavLink to="/">Home</NavLink>
   </div>
 );
 
-const Fallback = () => (
+const Fallback: React.SFC = () => (
   <div>
     Page not found
     <NavLink to="/">Home</NavLink>
   </div>
 );
 
-const App = () => (
+const App: React.SFC = () => (
   <Router>
     <div id="app-container">
       <Switch>
         <Route exact path="/" component={Welcome} />
-        <Route path="/test" component={Test} />
+        <Route path="/test" render={() => <Test testString="Test String" />} />
         <Route component={Fallback} />
       </Switch>
     </div>
