8ca477a149a1a9331ffec2e56833d26f1da6c7a5
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-07-15 23:29:34 -0400

    Updated Test component testing

--- src/components/test/index.tsx
+++ src/components/test/index.tsx
@@ -20,7 +20,7 @@ const Test: React.FC<TestProps> = ({ testString }) => (
       {testString}
     </div>
 
-    <NavLink to="/">Home</NavLink>
+    <NavLink to="/" role="navigation">Home</NavLink>
   </div>
 );
 
--- src/components/test/test.test.tsx
+++ src/components/test/test.test.tsx
@@ -1,11 +1,15 @@
 // Reference: https://testing-library.com/docs/react-testing-library/example-intro
+// Reference: https://blog.sapegin.me/all/react-testing-3-jest-and-react-testing-library/
+
+// Style reference: https://kentcdodds.com/blog/common-mistakes-with-react-testing-library
+
+// API reference: https://testing-library.com/docs/react-testing-library/cheatsheet
+// API reference: https://github.com/testing-library/jest-dom
 
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import {
-  render, fireEvent, screen,
-} from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Test from '.';
@@ -18,16 +22,27 @@ afterAll(() => {});
 function renderWithRouter(children: React.ReactElement) {
   const history = createMemoryHistory();
   return {
-    ...render(<Router history={history}>{children}</Router>),
+    ...render(
+      <Router history={history}>
+        {children}
+        <Route exact path="/">Hello, world!</Route>
+      </Router>,
+    ),
     history,
   };
 }
 
-test('displays loaded string correctly', () => {
+describe('Test', () => {
   const testMessage = 'Adam is cool';
 
-  renderWithRouter(<Test testString={testMessage} />);
-  expect(screen.getByText(new RegExp('Adam is cool', 'i'))).toBeVisible();
+  it('displays loaded string correctly', () => {
+    const { container } = renderWithRouter(<Test testString={testMessage} />);
+    expect(container.innerHTML).toMatch(new RegExp(testMessage, 'i'));
+  });
 
-  fireEvent.click(screen.getByText('Home'));
+  it('redirects correctly to the homepage', () => {
+    const { container } = renderWithRouter(<Test testString={testMessage} />);
+    fireEvent.click(screen.getByRole('navigation'), new MouseEvent('click', { bubbles: true }));
+    expect(container.innerHTML).toMatch(/Hello, world!/gi);
+  });
 });
